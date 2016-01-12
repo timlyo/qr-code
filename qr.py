724fc4ab2282fac714e633f0d03e08edf24df579
Tim Maddison <timmaddison@qinetiq.com>
2016-01-12 14:39:02 +0000

    Fixed render for new qr code
    resized it to 25*25

--- qr.py
+++ qr.py
@@ -1,4 +1,4 @@
-code = "ffbffff,80aee03,bebaafb,a286a8b,a26028b,a27da8b,be8cafb,806aa03,ff9e7ff,9c0079f,b6e6787,9218593,e12a3db,c9c6afb,a30d577,a098453,b9331ab,a45802f,ff46baf,8072a9b,be433bf,a2ba007,a28e253,a2a816b,be7703b,80431db,ffbffff,ffbffff"
+code = "3bb80,fbeabe,8b1aa2,8a80a2,8af6a2,fb32be,2aa80,1ff79ff,7101e7,dc99e1,496164,185a8f6,1281abe,8d355d,836114,e5cc6a,92600b,1fe1aeb,2caa6,fa0cef,8be801,8b3894,8ba05a,fadc0e,20c76"
 
 for thing in code.split(","):
-	print(format(int(thing, 16) + 262144, "028b").replace("1", "  ").replace("0", u"\u2588" * 2))
+	print(format(int(thing, 16), "025b").replace("1", "  ").replace("0", u"\u2588" * 2))
--- qr_to_numbers.py
+++ qr_to_numbers.py
@@ -1,12 +1,15 @@
 from PIL import Image
 
+import math
 import zlib, base64
+
 
 def bools_to_int(bools):
 	total = 0
 	for index, bool in enumerate(bools[::-1]):
 		total += bool * 2 ** index
 	return total
+
 
 # https://youtu.be/wxsvS_w36Es
 
@@ -17,14 +20,16 @@ image = Image.open(name).convert("L").point(lambda x: bool(x))
 
 pixels = image.getdata()
 
-lines = zip(*[iter(pixels)] * 28)
+print("Image is", math.sqrt(len(pixels)), "wide")
+
+lines = zip(*[iter(pixels)] * 25)
 
 digits = []
 
 largest_bit = 18
 
 for line in lines:
-	integer = bools_to_int(line) - 2**18 # magic number because none will be higher than it for given qr code, must be readded
+	integer = bools_to_int(line)  # magic number because none will be higher than it for given qr code, must be readded
 	string = str(hex(integer)).replace("0x", "")
 	# bytes = string.encode("utf-8")
 	digits.append(string)
